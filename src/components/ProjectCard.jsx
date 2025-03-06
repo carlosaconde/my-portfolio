809860a91489d0e58ba7899e1138c7640b2590e6
carlosaconde <carlos.andres.conde@gmail.com>
2025-03-06 01:00:44 -0300

    inclusion de video

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -19,10 +19,7 @@ export default function ProjectCard() {
     <Card sx={{ maxWidth: 350 }}>
       <CardContent>
         <video width="350" autoPlay loop>
-          <source
-            src="/my-portfolio/src/assets/buildhub.mp4"
-            type="video/mp4"
-          />
+          <source src="/my-portfolio/assets/buildhub.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
