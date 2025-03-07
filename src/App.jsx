d2352152d83aa3480a761e2d217a2a748f0c3d0f
carlosaconde <carlos.andres.conde@gmail.com>
2025-03-07 01:56:41 -0300

    cambios menores

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Contact } from "./components/Contact";
 import NavBar from "./components/NavBar";
 import { Projects } from "./components/Projects";
 import { Studies } from "./components/Studies";
-import { Tecnologies } from "./components/Tecnologies";
+import { Technologies } from "./components/Technologies";
 import { Vortex } from "./components/Vortex";
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <NavBar />
         <Projects />
         <Studies />
-        <Tecnologies />
+        <Technologies />
         <Contact />
       </Vortex>
     </>
